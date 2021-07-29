ad5cc6d2fab2e20ac1fd1c112662abf99c18fe29
Moses <moses.junior@student.moringaschool.com>
2021-07-29 19:16:06 +0300

    introduction of a html,css, js files,and IP3-master folder

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(".design img").click(function() {
+        $(".par1").toggle();
+
+
+    });
+    $(".design h3").click(function() {
+        $par(".par1").fadeIn();
+
+    });
+});
+$(document).ready(function() {
+    $(".development img").click(function() {
+        $(".par2").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".product img").click(function() {
+        $(".par3").toggle();
+    });
+});
